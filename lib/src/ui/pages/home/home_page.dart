0aacd37244673ca98ee70c7a4098a27fc2675c23
Murodjon <murodjonerkinov2005@gmail.com>
2023-11-27 23:38:31 +0500

    Add geolocation functionality and map interaction
    This change adds geolocation functionality to the home page of the app.
    Users can now grant permission to access their location, and the app
    will display the user's current location on a map using the Yandex Map
    SDK. Additionally, two floating action buttons allow the user to center
    the map on their current location and enable navigation mode. These new
    features enhance the user experience by providing location-based
    information and interaction. Fixes #123

--- lib/src/ui/pages/home/home_page.dart
+++ lib/src/ui/pages/home/home_page.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
-
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,10 +12,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<YandexMapController> _controller =
+      Completer<YandexMapController>();
+
+  Future<void> getPermission() async {
+    final permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      await Geolocator.requestPermission();
+    }
+  }
+
+  @override
+  void initState() {
+    getPermission();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: YandexMap(),
+      body: YandexMap(
+        onMapCreated: (controller) => _controller.complete(controller),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () async {
+              final controller = await _controller.future;
+              final current = await Geolocator.getCurrentPosition();
+              controller
+                  .moveCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                target: Point(
+                    latitude: current.latitude, longitude: current.longitude),
+                azimuth: current.heading,
+                zoom: 20,
+                tilt: 70,
+              )));
+            },
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () async {
+              final controller = await _controller.future;
+              final current = await Geolocator.getCurrentPosition();
+              controller
+                  .moveCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                target: Point(
+                    latitude: current.latitude, longitude: current.longitude),
+                azimuth: current.heading,
+                zoom: 20,
+                tilt: 70,
+              )));
+            },
+            child: const Icon(Icons.navigation_rounded),
+          ),
+        ],
+      ),
     );
   }
 }
