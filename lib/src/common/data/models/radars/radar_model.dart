e98ee4717467d4ca809560a975f6dbea0dd5f578
NorqobilovZafarbek <zafarbeknorqobilov360@gmail.com>
2023-12-01 20:00:19 +0500

    update_app

--- lib/src/common/data/models/radars/radar_model.dart
+++ lib/src/common/data/models/radars/radar_model.dart
@@ -1,4 +1,7 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 abstract class RadarModel {
+  final LatLng position;
   final int speed;
-  const RadarModel({required this.speed});
+  const RadarModel({required this.position, required this.speed});
 }
--- lib/src/common/data/models/radars/speed_radar.dart
+++ lib/src/common/data/models/radars/speed_radar.dart
@@ -1,5 +1,44 @@
 import 'package:antiradar/src/common/data/models/radars/radar_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SpeedRadar extends RadarModel {
-  SpeedRadar({required super.speed});
+  final String type;
+  final String direction;
+
+  SpeedRadar({
+    required this.type,
+    required this.direction,
+    required super.speed,
+    required super.position,
+  });
+
+  factory SpeedRadar.fromMap(Map<String, dynamic> map) {
+    return SpeedRadar(
+      type: map['type'] as String,
+      direction: map['direction'] as String,
+      speed: map['speed'] as int,
+      position: LatLng.fromJson((map['position'] as List).cast<double>())!,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'type': type,
+      'direction': direction,
+      'speed': speed,
+      'position': position.toJson(),
+    };
+  }
+
+  SpeedRadar copyWith({
+    String? type,
+    String? direction,
+    LatLng? position,
+    int? speed,
+  }) => SpeedRadar(
+      type: type ?? this.type,
+      direction: direction ?? this.direction,
+      position: position ?? this.position,
+      speed: speed ?? this.speed,
+    );
 }
